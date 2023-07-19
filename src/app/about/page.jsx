a03c946336d203c99e7093cd952b888294c7479b
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-07-20 00:17:39 +0500

    add about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,49 @@
+import Image from 'next/image'
 import React from 'react'
 import style from './page.module.css'
+import BannerImage from '../../../public/about-banner.jpg'
+import Button from '../../../components/Button/Button'
 
 const About = () => {
   return (
-    <div className={style.container}>About Page</div>
+    <div className={style.container}>
+      <div className={style.imgContainer}>
+        <Image
+          src={BannerImage}
+          fill={true}
+          alt='About us banner image'
+          className={style.img}
+        />
+        <div className={style.imgText}>
+          <h1 className={style.imgTitle}>Digital Storytellers</h1>
+          <h2 className={style.imgDesc}>Handcrafting award winning digital experiences</h2>
+        </div>
+      </div>
+      <div className={style.textContainer}>
+        <div className={style.item}>
+          <h1 className={style.title}>Who Are We?</h1>
+          <p className={style.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus deserunt deleniti ex, nisi unde atque.
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, eum tenetur. Rerum natus deserunt consectetur architecto minus laborum eveniet ab quo labore, veritatis nemo eum id laboriosam officia velit provident modi iure non tempore eaque quaerat quae eius voluptatum? Fuga.
+          </p>
+        </div>
+        <div className={style.item}>
+          <h1 className={style.title}>What We Do?</h1>
+          <p className={style.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, eum tenetur. Rerum natus deserunt consectetur architecto minus laborum eveniet ab quo labore, veritatis nemo eum id laboriosam officia velit provident modi iure non tempore eaque quaerat quae eius voluptatum? Fuga.
+            <br />
+            <br />
+            - Dynamic Websites
+            <br />
+            <br />
+            - Fast and Handy Mobile Apps
+          </p>
+          <Button text='contact' url='/contact' />
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import style from "./page.module.css";
 import HeroImage from "../../public/hero.png";
 import Image from "next/image";
+import Button from "../../components/Button/Button";
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
       <div className={style.item}>
         <h1 className={style.title}>Better design for your digital products.</h1>
         <p className={style.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis itaque porro voluptates quisquam asperiores laborum?</p>
-        <button className={style.button}>See Our Works</button>
+        <Button text='See Our Works' url='/portfolio' />
       </div>
       <div className={style.item}>
         <Image src={HeroImage} alt="Hero Image" className={style.img} />

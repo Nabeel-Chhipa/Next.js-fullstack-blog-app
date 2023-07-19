2ca38cbc40dd22501362b4d39fba5599f59cca78
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-07-19 23:20:50 +0500

    add hero section

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,12 +1,22 @@
+import Image from 'next/image'
 import React from 'react'
 import style from './footer.module.css'
+import FacebookIcon from '../../public/1.png'
+import InstaIcon from '../../public/2.png'
+import TwitterIcon from '../../public/3.png'
+import YoutubeIcon from '../../public/4.png'
 
 const Footer = () => {
   return (
-    <>
-      <div className={style.container}>@2023 Blogs. All rights reserved.</div>
-      <div></div>
-    </>
+    <div className={style.container}>
+      <div>@2023 Blogs. All rights reserved.</div>
+      <div className={style.social}>
+        <Image src={FacebookIcon} width={15} height={15} className={style.icon} alt='Facebook Icon' />
+        <Image src={InstaIcon} width={15} height={15} className={style.icon} alt='Instagram Icon' />
+        <Image src={TwitterIcon} width={15} height={15} className={style.icon} alt='Twitter Icon' />
+        <Image src={YoutubeIcon} width={15} height={15} className={style.icon} alt='Youtube Icon' />
+      </div>
+    </div>
   )
 }
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,18 @@
-import style from './page.module.css'
+import style from "./page.module.css";
+import HeroImage from "../../public/hero.png";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className={style.container}>Home</div>
-  )
+    <div className={style.container}>
+      <div className={style.item}>
+        <h1 className={style.title}>Better design for your digital products.</h1>
+        <p className={style.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis itaque porro voluptates quisquam asperiores laborum?</p>
+        <button className={style.button}>See Our Works</button>
+      </div>
+      <div className={style.item}>
+        <Image src={HeroImage} alt="Hero Image" className={style.img} />
+      </div>
+    </div>
+  );
 }

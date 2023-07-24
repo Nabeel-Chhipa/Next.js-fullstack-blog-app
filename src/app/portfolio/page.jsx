78816d3f44cdb7b74b1eaffb073f33029bf68e18
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-07-25 00:12:48 +0500

    add portfolio page

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,9 +1,23 @@
+import Link from 'next/link'
 import React from 'react'
 import style from './page.module.css'
 
 const Portfolio = () => {
   return (
-    <div className={style.container}>Portfolio Page</div>
+    <div className={style.container}>
+      <h1 className={style.title}>Choose a gallery</h1>
+      <div className={style.items}>
+        <Link href='/portfolio/illustrations' className={style.item}>
+          <span className={style.itemTitle}>Illustrations</span>
+        </Link>
+        <Link href='/portfolio/websites' className={style.item}>
+          <span className={style.itemTitle}>Websites</span>
+        </Link>
+        <Link href='/portfolio/applications' className={style.item}>
+          <span className={style.itemTitle}>Applications</span>
+        </Link>
+      </div>
+    </div>
   )
 }
 

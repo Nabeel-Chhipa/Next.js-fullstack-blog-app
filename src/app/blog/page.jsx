4c107bd861333cb91c2155f90695b6d87e6651b8
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-07-26 23:56:32 +0500

    get blog data from api

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,59 +1,43 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import style from './page.module.css'
-import imageOne from '../../../public/inner-blog-one.png'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import style from "./page.module.css";
+import imageOne from "../../../public/inner-blog-one.png";
 
-const Blog = () => {
-  return (
-    <div className={style.blogContainer}>
-      <Link href='/blog/testId' className={style.container}>
-        <div className={style.imgContainer}>
-          <Image
-            src={imageOne}
-            alt=''
-            width={400}
-            height={300}
-            className={style.image}
-          />
-        </div>
-        <div className={style.content}>
-          <h1 className={style.title}>Test</h1>
-          <p className={style.desc}>Description</p>
-        </div>
-      </Link>
-      <Link href='/blog/testId' className={style.container}>
-        <div className={style.imgContainer}>
-          <Image
-            src={imageOne}
-            alt=''
-            width={400}
-            height={300}
-            className={style.image}
-          />
-        </div>
-        <div className={style.content}>
-          <h1 className={style.title}>Test</h1>
-          <p className={style.desc}>Description</p>
-        </div>
-      </Link>
-      <Link href='/blog/testId' className={style.container}>
-        <div className={style.imgContainer}>
-          <Image
-            src={imageOne}
-            alt=''
-            width={400}
-            height={300}
-            className={style.image}
-          />
-        </div>
-        <div className={style.content}>
-          <h1 className={style.title}>Test</h1>
-          <p className={style.desc}>Description</p>
-        </div>
-      </Link>
-    </div>
-  )
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    throw new Error("Failed to load data");
+  }
+  return res.json();
 }
 
-export default Blog
+const Blog = async () => {
+  const data = await getData();
+
+  return (
+    <div className={style.blogContainer}>
+      {data.map((item) => (
+        <Link href="/blog/testId" className={style.container} key={item.id}>
+          <div className={style.imgContainer}>
+            <Image
+              src={imageOne}
+              alt=""
+              width={400}
+              height={300}
+              className={style.image}
+            />
+          </div>
+          <div className={style.content}>
+            <h1 className={style.title}>{item.title}</h1>
+            <p className={style.desc}>{item.body}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Blog;

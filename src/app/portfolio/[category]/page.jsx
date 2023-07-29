72f1744606f511d5c8fac8df3b00f82dbe4e1297
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-07-30 00:13:52 +0500

    fetch portfolio data with local json file

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -2,54 +2,43 @@ import Image from 'next/image'
 import React from 'react'
 import Button from '../../../../components/Button/Button'
 import style from './page.module.css'
-import illustrationsImageOne from '../../../../public/illustrations-inner-one.jpg'
+import { items } from './data'
+import { notFound } from 'next/navigation'
+
+const getPortfolio = (category) => {
+  const data = items[category]
+  if(data) {
+    return data
+  }
+  return notFound()
+}
 
 const Category = ({params}) => {
+  const data = getPortfolio(params.category)
+
   return (
     <div className={style.container}>
       <h1 className={style.cardTitle}>{params.category}</h1>
-      <div className={style.item}>
-        <div className={style.content}>
-          <h1 className={style.title}>Creative Portfolio</h1>
-          <p className={style.desc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, repellendus cum voluptates laboriosam ad aut! Sit porro provident voluptatibus accusamus error quasi animi non facere ad officia? Tempora, aut molestiae.</p>
-          <Button text='See More' url='#' />
-        </div>
-        <div className={style.imgContainer}>
-          <Image
-            src={illustrationsImageOne}
-            fill={true}
-            className={style.img}
-          />
-        </div>
-      </div>
-      <div className={style.item}>
-        <div className={style.content}>
-          <h1 className={style.title}>Creative Portfolio</h1>
-          <p className={style.desc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, repellendus cum voluptates laboriosam ad aut! Sit porro provident voluptatibus accusamus error quasi animi non facere ad officia? Tempora, aut molestiae.</p>
-          <Button text='See More' url='#' />
-        </div>
-        <div className={style.imgContainer}>
-          <Image
-            src={illustrationsImageOne}
-            fill={true}
-            className={style.img}
-          />
-        </div>
-      </div>
-      <div className={style.item}>
-        <div className={style.content}>
-          <h1 className={style.title}>Creative Portfolio</h1>
-          <p className={style.desc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam, repellendus cum voluptates laboriosam ad aut! Sit porro provident voluptatibus accusamus error quasi animi non facere ad officia? Tempora, aut molestiae.</p>
-          <Button text='See More' url='#' />
-        </div>
-        <div className={style.imgContainer}>
-          <Image
-            src={illustrationsImageOne}
-            fill={true}
-            className={style.img}
-          />
-        </div>
-      </div>
+      {
+        data.map(item => {
+          return (
+            <div className={style.item} key={item.id}>
+              <div className={style.content}>
+                <h1 className={style.title}>{item.title}</h1>
+                <p className={style.desc}>{item.desc}</p>
+                <Button text='See More' url='#' />
+              </div>
+              <div className={style.imgContainer}>
+                <Image
+                  src={item.image}
+                  fill={true}
+                  className={style.img}
+                />
+              </div>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
